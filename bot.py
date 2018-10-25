76abc76119ae876c1de47b0dc93e5181d7bf3afd
MePsyDuck <sumit231996@gmail.com>
2018-10-25 14:47:33 +0530

    Added individual counts

--- bot.py
+++ bot.py
@@ -28,36 +28,51 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    elif message.content.startswith('!oof_count'):
+
+    elif message.content.lower().startswith('!oof_count'):
         await client.send_message(message.channel, get_count())
         return
-    elif has_oof(message.content):
-        msg = '<:Oof:504616782695366657> count : {}'.format(update_count())
+
+    elif message.content.lower().startswith('!oof_king') or message.content.lower().startswith('!oof_queen'):
+        user_ids = get_max_oof_users()
+        msg = '<:Oof:504616782695366657> It\'s '
+        if len(user_ids) == 1:
+            user_name = await client.get_user_info(user_ids[0]).display_name
+            msg += '{}'.format(user_name)
+        else:
+            for user_id in user_ids[:-2]:
+                user_name = await client.get_user_info(user_id).display_name
+                msg += '{}, '.format(user_name)
+            second_last = await client.get_user_info(user_ids[-2]).display_name
+            last = await client.get_user_info(user_ids[-1]).display_name
+            msg += '{} and {}'.format(second_last, last)
         await client.send_message(message.channel, msg)
         return
-    elif message.content == '!rst_count':
-        reset_count()
-        await client.send_message(message.channel, 'Count reset to 0')
+
+    elif has_oof(message.content):
+        update_count(message.author.id)
+        msg = '<:Oof:504616782695366657> count : {}'.format(get_count())
+        await client.send_message(message.channel, msg)
         return
+
+
+def get_max_oof_users():
+    cur = conn.cursor()
+    max_user_query = 'SELECT user_id from oofcounttable WHERE oof_count = ' \
+                     '(SELECT MAX(oof_count) from oofcounttable WHERE user_id <> \'resv\' )'
+    cur.execute(max_user_query)
+    user_id_tuple = cur.fetchall()
+    user_ids = [user_id for user_id_row in user_id_tuple for user_id in user_id_row]
+    return user_ids
 
 
 def get_count():
     cur = conn.cursor()
-    get_query = 'SELECT oof_count FROM oofcounttable;'
+    get_query = 'SELECT sum(oof_count) FROM oofcounttable;'
     cur.execute(get_query)
     count = cur.fetchone()[0]
-    print('Get count : {}'.format(count))
-
     cur.close()
-
-
-def reset_count():
-    cur = conn.cursor()
-    reset_query = 'UPDATE oofcounttable SET oof_count = 0'
-    cur.execute(reset_query)
-    print('Set count to 0')
-
-    cur.close()
+    return count
 
 
 def has_oof(msg):
@@ -68,17 +83,11 @@ def has_oof(msg):
     return False
 
 
-def update_count():
+def update_count(user_id):
     cur = conn.cursor()
-    update_query = 'UPDATE oofcounttable SET oof_count = oof_count + 1'
-    cur.execute(update_query)
-    query = 'SELECT oof_count FROM oofcounttable;'
-    cur.execute(query)
-    count = cur.fetchone()[0]
-    print('New count is {}'.format(count))
-
+    update_query = 'UPDATE oofcounttable SET oof_count = oof_count + 1 WHERE user_id = %s'
+    cur.execute(update_query, (user_id,))
     cur.close()
-    return count
 
 
 client.run(TOKEN)
